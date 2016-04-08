2548cec41e590e80c764282afc198f555c7244cc
Daniel Dvorkin <Dvorkin212@gmail.com>
2016-04-07 20:34:57 -0400

    updating branch for heroku push

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],
-      :aws_secret_access_key  => ENV["AWS_SECRET_KEY"],
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],
       :region                 => 'us-east-1' # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = ENV["AWS_BUCKET_NAME"]
